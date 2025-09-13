6788e35b79a5b878394f72c7af35ba04585010c6
rohan-shenoy <70850246+rohan-shenoy@users.noreply.github.com>
2025-09-13 04:57:07 +0000

    Deploying to gh-pages from @ rohan-shenoy/rohan-shenoy.github.io@f0c0955f5a3583ee23b094ebdbf5a657b284f8aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,9 +445,9 @@ ninja.data = [{
           description: "Rohan Shenoy",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cs180-project-1-the-prokudin-gorskii-collection",
+          title: 'CS180 - Project 1. The Prokudin-Gorskii Collection',
+          description: "Rohan Shenoy",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
